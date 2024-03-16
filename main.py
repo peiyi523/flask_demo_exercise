c9ed4dfcd5399d9ea3a5fa9471fac63c3dded595
peiyi <peiyi523@gmail.com>
2024-03-16 20:57:28 +0800

    完成最高和最低的數值呈現跟showLoading & showLoading

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from datetime import datetime
-from pm25 import get_pm25
+from pm25 import get_pm25, get_six_pm25, six_countys
 import json
 
 
@@ -14,13 +14,41 @@ def pm25_charts():
     return render_template("pm25-charts.html")
 
 
+@app.route("/six-pm25-data")
+def six_pm25_data():
+    pm25 = get_six_pm25()
+    result = json.dumps(
+        {
+            "site": six_countys,
+            "pm25": pm25,
+        },
+        ensure_ascii=False,
+    )
+    return result
+
+
 @app.route("/pm25-data", methods=["GET"])
 def pm25_data():
     columns, values = get_pm25()
     site = [value[0] for value in values]
     pm25 = [value[2] for value in values]
     time = values[0][-2]
-    result = json.dumps({"time": time, "site": site, "pm25": pm25}, ensure_ascii=False)
+    # 取得最高跟最低的數據
+    sorted_data = sorted(values, key=lambda x: x[2])
+    print(sorted_data)
+    lowest = {"site": sorted_data[0][0], "pm25": sorted_data[0][2]}
+    highest = {"site": sorted_data[-1][0], "pm25": sorted_data[-1][2]}
+
+    result = json.dumps(
+        {
+            "time": time,
+            "site": site,
+            "pm25": pm25,
+            "lowest": lowest,
+            "highest": highest,
+        },
+        ensure_ascii=False,
+    )
 
     return result
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,67 @@
 
 // 圖表主繪製區域
-const myChart = echarts.init(document.getElementById('main'));
+const chart1 = echarts.init(document.getElementById('main'));
+const chart2 = echarts.init(document.getElementById('six'));
+$("#update").click(() => {
+    drawPM25();
+});
+
+
+
+// 先呼叫繪製全省
 drawPM25();
+// 再呼叫繪製六都
+drawSixPM25();
+
+// 取得後端資料
 function drawPM25() {
+    chart1.showLoading();
     $.ajax(
         {
             url: "/pm25-data",
             type: "GET",
             dataType: "json",
             success: (result) => {
-                console.log(result);
-                drawchart(myChart, result["time"], "PM2.5", result["site"], result["pm25"])
+                // 繪製對應區並給了必要參數
+                // jQuery寫法
+                $("#pm25_high_site").text(result["highest"]["site"]);
+                $("#pm25_high_value").text(result["highest"]["pm25"]);
+                $("#pm25_low_site").text(result["lowest"]["site"]);
+                $("#pm25_low_value").text(result["lowest"]["pm25"]);
+                drawchart(chart1, result["time"], "PM2.5", result["site"], result["pm25"])
+                chart1.hideLoading(close);
+                this.setTimeout(() => {
+                }, 1000);
+            },
+            error: () => {
+                alert("讀取資料失敗，請稍後再試!")
+                chart1.hideLoading();
             }
         }
     )
 }
+
+// 六都的函式
+function drawSixPM25() {
+    chart2.showLoading();
+    $.ajax(
+        {
+            url: "/six-pm25-data",
+            type: "GET",
+            dataType: "json",
+            success: (result) => {
+                // 繪製對應區並給了必要參數
+                drawchart(chart2, "六都PM2.5平均值", "PM2.5", result["site"], result["pm25"])
+                chart2.hideLoading(close);
+            },
+            error: () => {
+                alert("讀取資料失敗，請稍後再試!")
+                chart2.hideLoading();
+            }
+        }
+    )
+}
+
 
 function drawchart(chart, title, legend, xData, yData) {
     let option = {
